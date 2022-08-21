a3cc743fba94b879451bfd08018ad10b157ada56
Hemanth <nhemanthhyr@gmail.com>
2022-08-21 22:32:12 +0530

    Signin Route Added!

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+  res.send("Hello World");
+});
+
+app.get("/signin", (req, res) => {
+  res.send("Sign In");
+});
 
 app.listen(4000, () => console.log("Running on port http://localhost:4000"));
