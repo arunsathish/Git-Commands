12b9db7319be0dcd53040e79dfbac1e9875521d8
Arun S <arun3050@gmail.com>
2022-08-21 21:27:51 +0530

    Server Test OK!

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const app = require('express');
+const express = require("express");
 
+const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
 
 app.listen(4000, () => console.log("Running on port http://localhost:4000"));
